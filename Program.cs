e367ea6ece94adddf1b1c2f221bf4782b0e63738
vitalijtverskoj <vitalijtverskoj@gmail.com>
2024-02-22 08:22:50 +0300

    add solution

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿﻿string[] GetShortStringArray(string[] array)
+{
+    string strResult = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            strResult = strResult + array[i] + ";";
+        }
+    }
+    strResult = strResult.Remove(strResult.Length - 1);
+    string[] arrayResult = strResult.Split(";");
+    return arrayResult;
+}
+
+string[] strArray = {
+    "str",
+    "strong",
+    "st",
+    "stronger",
+    "s",
+    "strongless"
+};
+
+string [] shortStrArray = GetShortStringArray(strArray);
+Console.WriteLine($"[ {string.Join(", ", shortStrArray)} ]");
